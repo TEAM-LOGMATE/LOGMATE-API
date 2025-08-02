df7b5cad94411e3cccb47485505b6d4214d8b3e5
JJUYAAA <jjjjyy020704@naver.com>
2025-08-02 13:31:51 +0900

    [refactor] : User soft delete 처리 (#2)

--- src/main/java/com/logmate/user/model/User.java
+++ src/main/java/com/logmate/user/model/User.java
@@ -2,9 +2,13 @@ package com.logmate.user.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "users")
+@SQLDelete(sql = "UPDATE users SET active = false WHERE id = ?")
+@Where(clause = "active = true")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -22,4 +26,7 @@ public class User {
     private String password;
 
     private String name;
+
+    @Column(nullable = false)
+    private boolean active = true;
 }
