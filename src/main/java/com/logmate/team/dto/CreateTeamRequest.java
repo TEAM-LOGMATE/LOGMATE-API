fc52038fd098c35ff8c9b5f6ff3e8431795cab73
JJUYAAA <jjjjyy020704@naver.com>
2025-08-02 13:34:42 +0900

    [feat] : 팀 생성 dto 수정 - 팀 설명, 멤버 id 추가(#2)

--- src/main/java/com/logmate/team/dto/CreateTeamRequest.java
+++ src/main/java/com/logmate/team/dto/CreateTeamRequest.java
@@ -2,7 +2,11 @@ package com.logmate.team.dto;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class CreateTeamRequest {
     private String name;
+    private String description;
+    private List<Long> memberIds;
 }
