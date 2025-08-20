375289c8ccbeb202c92ddcee09ef29fabc5cb354
JJUYAAA <jjjjyy020704@naver.com>
2025-08-20 14:03:44 +0900

    [feat] : agentConfig 생성 (#2)

---
+++ src/main/java/com/logmate/agentConfig/dto/AgentConfig.java
@@ -0,0 +1,11 @@
+package com.logmate.agentConfig.dto;
+
+import lombok.Data;
+
+
+@Data
+public class AgentConfig {
+    private String agentId;
+    private String accessToken;
+    private String etag;
+}
