3e222dbfa9238c6e143c5d6262a8cafe823558b6
JJUYAAA <jjjjyy020704@naver.com>
2025-09-09 00:09:18 +0900

    [feat] : 대시보드에 개인 대시보드 여부를 판단할 수 있도록 user_id 추가, 소속 폴더를 나타내기 위한 folder_id 추가

--- src/main/java/com/logmate/dashboard/model/Dashboard.java
+++ src/main/java/com/logmate/dashboard/model/Dashboard.java
@@ -1,10 +1,11 @@
 package com.logmate.dashboard.model;
 
+import com.logmate.folder.Folder;
 import com.logmate.global.BaseEntity;
 import com.logmate.team.model.Team;
+import com.logmate.user.model.User;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
@@ -25,4 +26,12 @@ public class Dashboard extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "team_id")
     private Team team;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;   // 개인 대시보드용
+
+    @ManyToOne
+    @JoinColumn(name = "folder_id")
+    private Folder folder;  // 폴더 포함
 }
