87f8db35009bc7514fa27bc0e32dad02960dc6c9
JJUYAAA <jjjjyy020704@naver.com>
2025-09-15 18:22:47 +0900

    [refactor] : 파서 Config dto 수정 - detail Config를 파서 Config 내부로

--- src/main/java/com/logmate/agentConfig/dto/ParserConfig.java
+++ src/main/java/com/logmate/agentConfig/dto/ParserConfig.java
@@ -1,11 +1,20 @@
 package com.logmate.agentConfig.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 
 @Data
 public class ParserConfig {
     private String type;
     private ParserDetailConfig config;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ParserDetailConfig {
+        private String timestampPattern;
+        private String timezone;
+    }
 }
