d46d28b5b800e9632df3513bdad0fb1569da4acc
JJUYAAA <jjjjyy020704@naver.com>
2025-09-15 18:35:13 +0900

    [refactor] : 불필요한 코드 삭제

--- src/main/java/com/logmate/agentConfig/controller/AgentConfigController.java
+++ src/main/java/com/logmate/agentConfig/controller/AgentConfigController.java
@@ -1,13 +1,10 @@
 package com.logmate.agentConfig.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.logmate.agentConfig.dto.ConfigDTO;
 import com.logmate.agentConfig.dto.SaveDashboardConfigRequest;
-import com.logmate.agentConfig.repository.AgentConfigurationRepository;
 import com.logmate.agentConfig.service.AgentConfigService;
 import com.logmate.global.BaseResponse;
 import com.logmate.global.CustomException;
-import com.logmate.user.model.User;
 import com.logmate.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -24,19 +21,11 @@ public class AgentConfigController {
     private final AgentConfigService service;
     private final UserRepository userRepository;
 
-    private final AgentConfigurationRepository repository;
-    private final ObjectMapper objectMapper;
-
     @PostMapping("/{dashboardId}/config")
     public ResponseEntity<BaseResponse<Map<String, String>>> saveConfig(@PathVariable Long folderId,
                                                          @PathVariable Long dashboardId,
                                                          @RequestBody SaveDashboardConfigRequest request,
                                                          HttpServletRequest httpRequest) {
-        String email = (String) httpRequest.getAttribute("email");
-        User requester = userRepository.findByEmail(email)
-                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."));
-
-//        service.saveConfig("agent-" + dashboardId, request);
         String agentId = service.saveConfig(request);
 
         Map<String, String> response = Map.of("agentId", agentId);
