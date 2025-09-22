5c950eb6c0e496e97f2b1b077d2eec8d665f0aa3
JJUYAAA <jjjjyy020704@naver.com>
2025-09-22 10:59:39 +0900

    [feat] : LogPipelineConfig 레포지토리 생성

---
+++ src/main/java/com/logmate/agentConfig/repository/LogPipelineConfigRepository.java
@@ -0,0 +1,14 @@
+package com.logmate.agentConfig.repository;
+
+import com.logmate.agentConfig.model.AgentConfiguration;
+import com.logmate.agentConfig.model.LogPipelineConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LogPipelineConfigRepository extends JpaRepository<LogPipelineConfig, Long> {
+    List<LogPipelineConfig> findByAgentConfiguration(AgentConfiguration agentConfig);
+    LogPipelineConfig findByAgentConfigurationAndFilePath(AgentConfiguration agentConfig, String filePath);
+}
