0a841fa3de4cac689c735f6c5adb1c9b42d24bf2
JJUYAAA <jjjjyy020704@naver.com>
2025-09-24 17:08:35 +0900

    [refactor] : 대시보드 고급정보 저장 dto 수정 - agentId, pushUrl 수정 및 logpipelineConfigs 추가

--- src/main/java/com/logmate/agentConfig/dto/SaveDashboardConfigRequest.java
+++ src/main/java/com/logmate/agentConfig/dto/SaveDashboardConfigRequest.java
@@ -2,14 +2,15 @@ package com.logmate.agentConfig.dto;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class SaveDashboardConfigRequest {
+    private String agentId; // NULL 이면 신규
 
     @Data
     public static class TailerRequest {
+        private String filePath; // 요청시에 한번 더 받아야 할듯
         private int readIntervalMs;
         private String metaDataFilePathPrefix;
     }
@@ -22,6 +23,7 @@ public class SaveDashboardConfigRequest {
 
     @Data
     public static class ExporterRequest {
+        private String pushURL; //streaming server 경로로 서버에서 채워주기
         private Boolean compressEnabled;
         private int retryIntervalSec;
         private int maxRetryCount;
@@ -29,19 +31,33 @@ public class SaveDashboardConfigRequest {
 
     @Data
     public static class FilterRequest {
+        // SpringBoot
         private List<String> allowedLevels;
         private List<String> requiredKeywords;
-        private LocalDateTime after;
+
+        // Tomcat
+        private List<String> allowedMethods;
     }
 
     @Data
     public static class PullerRequest {
-        private int intervalSec;
     }
 
-    private TailerRequest tailer;
-    private MultilineRequest multiline;
-    private ExporterRequest exporter;
-    private FilterRequest filter;
+    @Data
+    public static class ParserConfigRequest {
+        private String timezone; //e.g. Asia/Seoul
+    }
+
+    @Data
+    public static class WatcherRequest {
+        private TailerRequest tailer;
+        private MultilineRequest multiline;
+        private ExporterRequest exporter;
+        private FilterRequest filter;
+        private String parserType; // "springboot" or "tomcat"
+        private ParserConfig.ParserDetailConfig parser;
+    }
+
     private PullerRequest puller;
+    private List<WatcherRequest> logPipelineConfigs;
 }
