c1b93d7f52a857a2ff8b9b9c7f27666a5ea95a84
JJUYAAA <jjjjyy020704@naver.com>
2025-10-16 01:06:13 +0900

    [feat] : 프론트 새로운 도메인 추가

--- src/main/java/com/logmate/global/config/WebConfig.java
+++ src/main/java/com/logmate/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //모든 엔드포인트
-                .allowedOrigins("http://localhost:3000",  "http://localhost:4173", "http://3.34.198.253")
+                .allowedOrigins("http://localhost:3000",  "http://localhost:4173", "http://3.34.198.253", "https://log-mate-site.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
