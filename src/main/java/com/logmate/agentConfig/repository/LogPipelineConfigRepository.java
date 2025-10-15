b8654e74df99cbc280ae5c5980acc7d02349cb53
JJUYAAA <jjjjyy020704@naver.com>
2025-10-16 01:51:12 +0900

    [feat] : 대시보드Id로 logpipelineConfig 조회 가능하도록 추가

--- src/main/java/com/logmate/agentConfig/repository/LogPipelineConfigRepository.java
+++ src/main/java/com/logmate/agentConfig/repository/LogPipelineConfigRepository.java
@@ -3,6 +3,8 @@ package com.logmate.agentConfig.repository;
 import com.logmate.agentConfig.model.AgentConfiguration;
 import com.logmate.agentConfig.model.LogPipelineConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +13,12 @@ import java.util.List;
 public interface LogPipelineConfigRepository extends JpaRepository<LogPipelineConfig, Long> {
     List<LogPipelineConfig> findByAgentConfiguration(AgentConfiguration agentConfig);
     LogPipelineConfig findByAgentConfigurationAndFilePath(AgentConfiguration agentConfig, String filePath);
-    List<LogPipelineConfig> findByDashboardId(Long dashboardId);
+    @Query("""
+SELECT l FROM LogPipelineConfig l 
+JOIN l.agentConfiguration a 
+WHERE l.dashboardId = :dashboardId
+""")
+    List<LogPipelineConfig> findByDashboardId(@Param("dashboardId") Long dashboardId);
     LogPipelineConfig findByAgentConfigurationAndFilePathAndDashboardId(
             AgentConfiguration agentConfig, String filePath, Long dashboardId);
     List<LogPipelineConfig> findByDashboardIdIn(List<Long> dashboardIds);
