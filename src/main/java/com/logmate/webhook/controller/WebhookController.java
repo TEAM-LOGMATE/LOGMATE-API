33ae67e416017e76c1e09eb92384a802bc0fd038
JJUYAAA <jjjjyy020704@naver.com>
2025-10-15 16:38:48 +0900

    [feat] : 프론트 새로운 도메인 추가

--- src/main/java/com/logmate/webhook/controller/WebhookController.java
+++ src/main/java/com/logmate/webhook/controller/WebhookController.java
@@ -1,5 +1,6 @@
 package com.logmate.webhook.controller;
 
+import com.logmate.global.BaseResponse;
 import com.logmate.global.CustomException;
 import com.logmate.user.model.User;
 import com.logmate.user.repository.UserRepository;
@@ -24,26 +25,27 @@ public class WebhookController {
 
     //등록
     @PostMapping
-    public ResponseEntity<WebhookResponseDto> register(
+    public ResponseEntity<BaseResponse<WebhookResponseDto>> register(
             @RequestBody WebhookRequestDto dto,
             HttpServletRequest request
     ) {
         String email = (String) request.getAttribute("email");
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new SecurityException("인증되지 않은 사용자입니다"));
-        return ResponseEntity.ok(webhookService.register(user.getId(), dto));
+                .orElseThrow(() -> new CustomException(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."));
+        WebhookResponseDto saved = webhookService.register(user.getId(), dto);
+        return ResponseEntity.ok(BaseResponse.of(200, "Webhook 등록 성공", saved));
     }
 
     //테스트
     @PostMapping("/test")
-    public ResponseEntity<Void> testWebhook(@RequestParam String url) {
+    public ResponseEntity<BaseResponse<Void>> testWebhook(@RequestParam String url) {
         webhookService.testSend(url);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(BaseResponse.of(200, "Webhook 테스트 전송 성공",null));
     }
 
     //등록된 Webhook으로 알림 전송
     @PostMapping("/trigger")
-    public ResponseEntity<Void> trigger(
+    public ResponseEntity<BaseResponse<Void>> trigger(
             @RequestParam String message,
             HttpServletRequest request
     ) {
@@ -52,23 +54,23 @@ public class WebhookController {
                 .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."));
 
         webhookService.sendEventToUserWebhooks(user.getId(), message);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(BaseResponse.of(200, "Webhook 알림 전송 성공",null));
     }
 
     //본인의 웹훅 url 조회
     @GetMapping
-    public ResponseEntity<List<WebhookResponseDto>> getUserWebhooks(HttpServletRequest request) {
+    public ResponseEntity<BaseResponse<List<WebhookResponseDto>>> getUserWebhooks(HttpServletRequest request) {
         String email = (String) request.getAttribute("email");
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."));
 
         List<WebhookResponseDto> webhooks = webhookService.getUserWebhooks(user.getId());
-        return ResponseEntity.ok(webhooks);
+        return ResponseEntity.ok(BaseResponse.of(200, "Webhook 목록 조회 성공",webhooks));
     }
 
     //Webhook 삭제
     @DeleteMapping("/{webhookId}")
-    public ResponseEntity<Void> deleteWebhook(
+    public ResponseEntity<BaseResponse<Void>> deleteWebhook(
             @PathVariable Long webhookId,
             HttpServletRequest request
     ) {
@@ -77,7 +79,6 @@ public class WebhookController {
                 .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."));
 
         webhookService.deleteWebhook(user.getId(), webhookId);
-        return ResponseEntity.noContent().build(); // 204
-
+        return ResponseEntity.ok(BaseResponse.of(200, "Webhook 삭제 성공", null));
     }
 }
