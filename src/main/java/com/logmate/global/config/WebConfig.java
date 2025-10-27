4a4fab81d88bfc3c51da45fd83936c4c4479a717
JJUYAAA <jjjjyy020704@naver.com>
2025-10-27 20:02:38 +0900

    [refactor] : CORS에 새로운 엔드포인트 추가 및 필요없는 엔드포인트 제거

--- src/main/java/com/logmate/global/config/WebConfig.java
+++ src/main/java/com/logmate/global/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //모든 엔드포인트
-                .allowedOrigins("http://localhost:3000",  "http://localhost:4173", "http://3.34.198.253", "https://log-mate-site.netlify.app")
+                .allowedOrigins("http://localhost:3000",  "http://localhost:4173",
+                        "https://log-mate-site.netlify.app","https://logmate.shop", "https://www.logmate.shop")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
